4c167d2f12d4021dbb24359297a383a9d663203f
bendgk <benkosten@gmail.com>
2018-10-27 05:19:03 -0500

    Add basic user login page

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,8 +1,18 @@
-function create_new_user(){
+function create_new_user() {
     console.log("Button was pressed");
     $.ajax({
         type: "POST",
         url: '/create_user',
         data: {'data': 1, 'data2': 123, 'array1': [1, 2, 3]},
       });
+}
+
+function displayLogin() {
+    $(".register-body").hide();
+    $(".login-body").show();
+}
+
+function displayRegister() {
+    $(".login-body").hide();
+    $(".register-body").show();
 }
--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//mongodb stuff
+mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "connection error: "));
+db.once('open', () => {
+    console.log("Connected to DB!");
+    
+    var userSchema = new mongoose.Schema({
+        name: String,
+        password: String
+    });
+    
+    var User = mongoose.model('User', userSchema);
+});
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded());
 
 // This responds a POST request for the homepage
-app.post('/create_user', function (req, res) {
+app.post('/register', (req, res) => {
    console.log(req.body);
-})
+   res.redirect("/");
+});
 
-var server = app.listen(8081, function () {
-
-   var host = server.address().address
-   var port = server.address().port
+var server = app.listen(8081, () => {
+   const host = server.address().address
+   const port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
-})
+});
