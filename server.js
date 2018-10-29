de7a5fde4fd4d2c51a075ebc743e0c3d8c9a8c24
bendgk <benkosten@gmail.com>
2018-10-28 21:41:25 -0500

    Add user registration

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 const db = mongoose.connection;
 
+//models
+var User;
+
 db.on('error', console.error.bind(console, "connection error: "));
 db.once('open', () => {
     console.log("Connected to DB!");
@@ -18,12 +21,13 @@ db.once('open', () => {
             unique: true,
             required: true,
             trim: true
-        }
+        },
         
         password: {
             type: String,
             required: true
-        }
+        },
+        
         email: {
             type: String,
             unique: true,
@@ -32,7 +36,8 @@ db.once('open', () => {
         }
     });
     
-    var User = mongoose.model('User', userSchema);
+    User = mongoose.model('User', userSchema);
+    
 });
 
 app.use(express.static('public'))
@@ -42,6 +47,30 @@ app.use(bodyParser.urlencoded());
 app.post('/register', (req, res) => {
    console.log(req.body);
    
+   User.find({ email: req.body['email'] }, (err, users) => {
+       if (err) {
+           console.error(err);
+       } else {
+           console.log(users);
+            if (users.length == 0) {
+                var user = new User({
+                    name: req.body['user'],
+                    password: req.body['pass'],
+                    email: req.body['email']
+                })
+                
+                user.save((err, user) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(400).send("Bad Request");
+                    }
+                });
+            }
+            else {
+                console.log(`${req.body['email']} already registered!`);
+            }
+       }
+   });
    res.redirect("/");
 });
 
